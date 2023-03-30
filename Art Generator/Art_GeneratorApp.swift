b35eba51cd96e8671b61457ea59f484331749944
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-03-30 17:52:33 -0400

    Initial Commit

---
+++ Art Generator/Art_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Art_GeneratorApp.swift
+//  Art Generator
+//
+//  Created by Leonard McCook-Carr on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Art_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Art Generator/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Art Generator
+//
+//  Created by Leonard McCook-Carr on 3/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, World!")
+            
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
