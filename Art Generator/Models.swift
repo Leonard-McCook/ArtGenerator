2966fc3976bd3fdba5808f556a60312eeb089332
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-03-31 18:33:54 -0400

    Create Models file

---
+++ Art Generator/Models.swift
@@ -0,0 +1,15 @@
+//
+//  Models.swift
+//  Art Generator
+//
+//  Created by Leonard McCook-Carr on 3/31/23.
+//
+
+import Foundation
+
+enum Constants {
+    static let imageSize = "256x256"
+    static let n = 1
+}
+
+
